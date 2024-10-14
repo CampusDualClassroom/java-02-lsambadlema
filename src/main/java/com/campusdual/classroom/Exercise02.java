cc59acd1b30a88b9548564a90230aee9dfe3445a
Lorena Sambad Lema <lsambadlema@gmail.com>
2024-10-14 13:20:37 +0200

    Terminado Exercise02Test

--- src/main/java/com/campusdual/classroom/Exercise02.java
+++ src/main/java/com/campusdual/classroom/Exercise02.java
@@ -6,12 +6,13 @@ public class Exercise02 {
         double r = 15;
         double a, l;
 
-        // TODO: ↓ Fórmula del área de un círculoS: a = radio * radio * Número PI
 
+        // TODO: ↓ Fórmula del área de un círculo: a = radio * radio * Número PI
+        a = Math.PI * Math.pow(r, 2);
         System.out.println("Area: " + a);
 
         // TODO: ↓ Fórmula de la longitud de una circunferencia: l = 2 * radio * Número PI
-
+        l = 2 * Math.PI * r;
         System.out.println("Longitud:  " + l);
 
     }
